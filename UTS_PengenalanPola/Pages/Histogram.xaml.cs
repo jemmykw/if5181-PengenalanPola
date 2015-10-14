63f6e7d6e2002e7de723a4c317e78d84f041410b
Vincentius Timothy <vincentius@students.itb.ac.id>
2015-10-14 16:33:06 +0700

    Histogram sudah bisa meskipun UI-nya badai

--- UTS_PengenalanPola/Pages/Histogram.xaml.cs
+++ UTS_PengenalanPola/Pages/Histogram.xaml.cs
@@ -28,54 +28,230 @@ namespace UTS_PengenalanPola.Pages
 
         private void OpenButton_Click(object sender, RoutedEventArgs e)
         {
-            //var fileDialog = new System.Windows.Forms.OpenFileDialog();
-            //fileDialog.Filter = "Image Files (*.bmp)|*.bmp";
-            //fileDialog.FilterIndex = 1;
-            //var result = fileDialog.ShowDialog();
+            var fileDialog = new System.Windows.Forms.OpenFileDialog();
+            fileDialog.Filter = "Image Files (*.png)|*.png";
+            fileDialog.FilterIndex = 1;
+            var result = fileDialog.ShowDialog();
 
-            //if (result == System.Windows.Forms.DialogResult.OK)
-            //{
-            //    var file = fileDialog.FileName;
-            //    FileInfo fi = new FileInfo(file);
-            //    TxtFile.Text = fi.Extension;
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
 
-            //    // Open a Uri and decode a BMP image
-            //    Uri myUri = new Uri(file, UriKind.RelativeOrAbsolute);
-            //    BmpBitmapDecoder decoder2 = new BmpBitmapDecoder(myUri, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
-            //    BitmapSource bitmapSource2 = decoder2.Frames[0];
-
-            //    // Draw the Image
-            //    myImage2.Source = bitmapSource2;
-            //    myImage2.Stretch = Stretch.Fill;
-
-            BitmapImage originalIamge = new BitmapImage();
-            originalIamge.BeginInit();
-            originalIamge.UriSource = new Uri("http://270c81.medialib.glogster.com/aidallanos/media/7d/7da3f80f5680169c6520d8bccfa781b9d452325c/ks-color.png");
-            originalIamge.DownloadCompleted += delegate {
-                BitmapSource bitmapSource = new FormatConvertedBitmap(originalIamge, PixelFormats.Pbgra32, null, 0);
+                BitmapImage originalIamge = new BitmapImage();
+                originalIamge.BeginInit();
+                originalIamge.UriSource = new Uri(fileDialog.FileName, UriKind.RelativeOrAbsolute);
+                PngBitmapDecoder decoder2 = new PngBitmapDecoder(originalIamge.UriSource, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+                BitmapSource bitmapSource = decoder2.Frames[0];
                 WriteableBitmap modifiedImage = new WriteableBitmap(bitmapSource);
 
-                int h = modifiedImage.PixelHeight;
-                int w = modifiedImage.PixelWidth;
-                int[] pixelData = new int[w * h];
-                int widthInByte = 4 * w;
-
-                modifiedImage.CopyPixels(pixelData, widthInByte, 0);
-
-                for (int i = 0; i < pixelData.Length; i++)
+                int stride = bitmapSource.PixelWidth * 4;
+                int size = bitmapSource.PixelHeight * stride;
+                byte[] rawPixels = new byte[size];
+                bitmapSource.CopyPixels(rawPixels, stride, 0);
+                
+                for (int i = 0; i < rawPixels.Length; i++)
                 {
-                    pixelData[i] ^= 0x00ffffff;
+                    OriginalPixel.Text += "\n" + i + " = " + rawPixels[i];
                 }
 
-                modifiedImage.WritePixels(new Int32Rect(0, 0, w, h), pixelData, widthInByte, 0);
+                byte[] sPixels = transform(rawPixels);
+
+                for (int i = 0; i < sPixels.Length; i++)
+                {
+                    ModifiedPixel.Text += "\n" + i + " = " + sPixels[i];
+                }
+
+                modifiedImage.WritePixels(new Int32Rect(0, 0, bitmapSource.PixelWidth, bitmapSource.PixelHeight), sPixels, stride, 0);
 
                 ModifiedImage.Source = modifiedImage;
-            };
 
-            originalIamge.EndInit();
-            OriginalImage.Source = originalIamge;
+                originalIamge.EndInit();
+                OriginalImage.Source = originalIamge;
+            }
 
+        }
 
+        private byte[] transform(byte[] rawPixels)
+        {
+            int ncol = 256;
+            int rawPixelsLength = rawPixels.Length;
+            double eachColLength = (double)rawPixelsLength / 4;
+
+            int[] blueArray = new int[ncol];
+            int[] greenArray = new int[ncol];
+            int[] redArray = new int[ncol];
+            int[] alphaArray = new int[ncol];
+
+            for (int i = 0; i < rawPixelsLength; i++)
+            {
+                switch (i % 4)
+                {
+                    case 0: // blue
+                        blueArray[rawPixels[i]]++;
+                        break;
+                    case 1: // green
+                        greenArray[rawPixels[i]]++;
+                        break;
+                    case 2: // merah
+                        redArray[rawPixels[i]]++;
+                        break;
+                    case 3: // alpha
+                        alphaArray[rawPixels[i]]++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            PointCollection bluePoints = new PointCollection();
+            PointCollection greenPoints = new PointCollection();
+            PointCollection redPoints = new PointCollection();
+            PointCollection alphaPoints = new PointCollection();
+
+            Point p = new Point();
+
+            double blueArrayMax = (double)blueArray.Max();
+            double greenArrayMax = (double)greenArray.Max();
+            double redArrayMax = (double)redArray.Max();
+            double alphaArrayMax = (double)alphaArray.Max();
+
+            p.X = (double)(0);
+            p.Y = (double)(BlueHistogram.Height * (1 - blueArray[0] / blueArrayMax));
+            bluePoints.Add(p);
+            p.Y = (double)(GreenHistogram.Height * (1 - greenArray[0] / greenArrayMax));
+            greenPoints.Add(p);
+            p.Y = (double)(RedHistogram.Height * (1 - redArray[0] / redArrayMax));
+            redPoints.Add(p);
+            p.Y = (double)(AlphaHistogram.Height * (1 - alphaArray[0] / alphaArrayMax));
+            alphaPoints.Add(p);
+
+            double[] blueAccumulator = new double[ncol];
+            double[] greenAccumulator = new double[ncol];
+            double[] redAccumulator = new double[ncol];
+            double[] alphaAccumulator = new double[ncol];
+
+            byte[] blueALU = new byte[ncol];
+            byte[] greenALU = new byte[ncol];
+            byte[] redALU = new byte[ncol];
+            byte[] alphaALU = new byte[ncol];
+
+            blueAccumulator[0] = blueArray[0] / eachColLength;
+            greenAccumulator[0] = greenArray[0] / eachColLength;
+            redAccumulator[0] = redArray[0] / eachColLength;
+            alphaAccumulator[0] = alphaArray[0] / eachColLength;
+
+            blueALU[0] = (byte)Math.Round((decimal)((ncol - 1) * blueAccumulator[0]));
+            greenALU[0] = (byte)Math.Round((decimal)((ncol - 1) * greenAccumulator[0]));
+            redALU[0] = (byte)Math.Round((decimal)((ncol - 1) * redAccumulator[0]));
+            alphaALU[0] = (byte)Math.Round((decimal)((ncol - 1) * alphaAccumulator[0]));
+
+            for (int i = 1; i < ncol; i++)
+            {
+                p.X = (double)(i);
+                p.Y = (double)(BlueHistogram.Height * (1 - blueArray[i] / blueArrayMax));
+                bluePoints.Add(p);
+                p.Y = (double)(GreenHistogram.Height * (1 - greenArray[i] / greenArrayMax));
+                greenPoints.Add(p);
+                p.Y = (double)(RedHistogram.Height * (1 - redArray[i] / redArrayMax));
+                redPoints.Add(p);
+                p.Y = (double)(AlphaHistogram.Height * (1 - alphaArray[i] / alphaArrayMax));
+                alphaPoints.Add(p);
+
+                blueAccumulator[i] = blueAccumulator[i - 1] + blueArray[i] / eachColLength;
+                greenAccumulator[i] = greenAccumulator[i - 1] + greenArray[i] / eachColLength;
+                redAccumulator[i] = redAccumulator[i - 1] + redArray[i] / eachColLength;
+                alphaAccumulator[i] = alphaAccumulator[i - 1] + alphaArray[i] / eachColLength;
+
+                blueALU[i] = (byte)Math.Round((decimal)((ncol - 1) * blueAccumulator[i]));
+                greenALU[i] = (byte)Math.Round((decimal)((ncol - 1) * greenAccumulator[i]));
+                redALU[i] = (byte)Math.Round((decimal)((ncol - 1) * redAccumulator[i]));
+                alphaALU[i] = (byte)Math.Round((decimal)((ncol - 1) * alphaAccumulator[i]));
+            }
+
+            BlueHistogram.Points = bluePoints;
+            GreenHistogram.Points = greenPoints;
+            RedHistogram.Points = redPoints;
+            AlphaHistogram.Points = alphaPoints;
+
+            byte[] sPixels = new byte[rawPixelsLength];
+
+            for (int i = 0; i < rawPixelsLength; i++)
+            {
+                switch (i % 4)
+                {
+                    case 0: // blue
+                        sPixels[i] = blueALU[rawPixels[i]];
+                        break;
+                    case 1: // green
+                        sPixels[i] = greenALU[rawPixels[i]];
+                        break;
+                    case 2: // merah
+                        sPixels[i] = redALU[rawPixels[i]];
+                        break;
+                    case 3: // alpha
+                        sPixels[i] = alphaALU[rawPixels[i]];
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            // New Histogram
+
+            blueArray = new int[ncol];
+            greenArray = new int[ncol];
+            redArray = new int[ncol];
+            alphaArray = new int[ncol];
+
+            for (int i = 0; i < rawPixelsLength; i++)
+            {
+                switch (i % 4)
+                {
+                    case 0: // blue
+                        blueArray[sPixels[i]]++;
+                        break;
+                    case 1: // green
+                        greenArray[sPixels[i]]++;
+                        break;
+                    case 2: // merah
+                        redArray[sPixels[i]]++;
+                        break;
+                    case 3: // alpha
+                        alphaArray[sPixels[i]]++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            bluePoints = new PointCollection();
+            greenPoints = new PointCollection();
+            redPoints = new PointCollection();
+            alphaPoints = new PointCollection();
+
+            blueArrayMax = (double)blueArray.Max();
+            greenArrayMax = (double)greenArray.Max();
+            redArrayMax = (double)redArray.Max();
+            alphaArrayMax = (double)alphaArray.Max();
+
+            for (int i = 0; i < ncol; i++)
+            {
+                p.X = (double)(i);
+                p.Y = (double)(NewBlueHistogram.Height * (1 - blueArray[i] / blueArrayMax));
+                bluePoints.Add(p);
+                p.Y = (double)(NewGreenHistogram.Height * (1 - greenArray[i] / greenArrayMax));
+                greenPoints.Add(p);
+                p.Y = (double)(NewRedHistogram.Height * (1 - redArray[i] / redArrayMax));
+                redPoints.Add(p);
+                p.Y = (double)(NewAlphaHistogram.Height * (1 - alphaArray[i] / alphaArrayMax));
+                alphaPoints.Add(p);
+            }
+
+            NewBlueHistogram.Points = bluePoints;
+            NewGreenHistogram.Points = greenPoints;
+            NewRedHistogram.Points = redPoints;
+            NewAlphaHistogram.Points = alphaPoints;
+
+            return sPixels;
         }
 
     }
