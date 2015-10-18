fe1d9db269268febafe92644f4c1a1368732c3b5
jemmy kristianto <jemmy.kw@gmail.com>
2015-10-19 04:59:59 +0700

    inisial thinning code

--- UTS_PengenalanPola/Pages/ThinningCode.xaml.cs
+++ UTS_PengenalanPola/Pages/ThinningCode.xaml.cs
@@ -30,23 +30,23 @@ namespace UTS_PengenalanPola.Pages
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var fileDialog = new System.Windows.Forms.OpenFileDialog();
-            fileDialog.Filter = "Image Files (*.bmp)|*.bmp";
-            fileDialog.FilterIndex = 1;
-            var result = fileDialog.ShowDialog();
-            switch (result)
-            {
-                case System.Windows.Forms.DialogResult.OK:
-                    var file = fileDialog.FileName;
-                    TxtFile.Text = file;
-                    TxtFile.ToolTip = file;
-                    break;
-                case System.Windows.Forms.DialogResult.Cancel:
-                default:
-                    TxtFile.Text = null;
-                    TxtFile.ToolTip = null;
-                    break;
-            }
+            //var fileDialog = new System.Windows.Forms.OpenFileDialog();
+            //fileDialog.Filter = "Image Files (*.bmp)|*.bmp";
+            //fileDialog.FilterIndex = 1;
+            //var result = fileDialog.ShowDialog();
+            //switch (result)
+            //{
+            //    case System.Windows.Forms.DialogResult.OK:
+            //        var file = fileDialog.FileName;
+            //        TxtFile.Text = file;
+            //        TxtFile.ToolTip = file;
+            //        break;
+            //    case System.Windows.Forms.DialogResult.Cancel:
+            //    default:
+            //        TxtFile.Text = null;
+            //        TxtFile.ToolTip = null;
+            //        break;
+            //}
 
 
             // Open a Uri and decode a BMP image
@@ -55,7 +55,7 @@ namespace UTS_PengenalanPola.Pages
             //BitmapSource My_Image = decoder2.Frames[0];
 
             //codeEmgucv
-            Image<Bgr, byte> My_Image = new Image<Bgr, byte>(fileDialog.FileName);
+            //Image<Bgr, byte> My_Image = new Image<Bgr, byte>(fileDialog.FileName);
             //myImage2.Source = My_Image.ToBitmap();
             // Draw the Image
             //Image myImage2 = new Image();
@@ -63,6 +63,47 @@ namespace UTS_PengenalanPola.Pages
             //myImage2.Source = My_Image.ToBitmap(); 
             //myImage2.Stretch = Stretch.None;
             //myImage2.Margin = new Thickness(20);
+
+            var fileDialog = new System.Windows.Forms.OpenFileDialog();
+            fileDialog.Filter = "Image Files (*.png)|*.png";
+            fileDialog.FilterIndex = 1;
+            var result = fileDialog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+
+                BitmapImage originalImage = new BitmapImage();
+                originalImage.BeginInit();
+
+                originalImage.UriSource = new Uri(fileDialog.FileName);
+                PngBitmapDecoder decoder = new PngBitmapDecoder(originalImage.UriSource, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+
+                BitmapSource bitmapSource = decoder.Frames[0];
+
+                WriteableBitmap modifiedImage = new WriteableBitmap(bitmapSource);
+
+                int stride = bitmapSource.PixelWidth * 4;
+                int size = bitmapSource.PixelHeight * stride;
+                byte[] pixelData = new byte[size];
+
+                modifiedImage.CopyPixels(pixelData, stride, 0);
+
+                //drawOriginalHistogram(pixelData);
+                //pixelData = equalize(pixelData);
+                //drawModifiedHistogram(pixelData);
+
+                modifiedImage.WritePixels(new Int32Rect(0, 0, bitmapSource.PixelWidth, bitmapSource.PixelHeight), pixelData, stride, 0);
+
+                ModifiedImage.Source = modifiedImage;
+
+                originalImage.EndInit();
+                OriginalImage.Source = originalImage;
+            }
+        }//end method
+
+        private void ImageCheckToBool(byte[] pixelData)
+        {
+            
         }
     }
 }
